fcf0f1045ee821fcbac6d70d01913b3143e92e98
yoelbin <yoelbin@gmail.com>
2022-01-10 13:33:17 +0200

    Updates

---
+++ precache-manifest.38cb5093ae065c2631faeada276b1ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff4789320a66b1a906ea13f87bbd34c",
+    "url": "/Musix/index.html"
+  },
+  {
+    "revision": "51578549534fa51df663",
+    "url": "/Musix/static/js/2.c01582d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Musix/static/js/2.c01582d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71819562c4eb17eb34cf",
+    "url": "/Musix/static/js/main.1f7a9980.chunk.js"
+  },
+  {
+    "revision": "ed5638173bc09c8c9480",
+    "url": "/Musix/static/js/runtime-main.c72f0ffe.js"
+  }
+]);
